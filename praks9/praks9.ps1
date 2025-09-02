c64dc5d8f26364986e04b6b02648fa6b5b0b829d
Aleksander Püüa <aleksander.puua@voco.ee>
2025-09-02 17:23:20 +0300

    Do While Loop

---
+++ praks9/praks9.ps1
@@ -0,0 +1,22 @@
+﻿## Teeme sarnase skripti nagu eelmises praksis, kuid seekord näitame ka ära mitu korda tuli teavitus "Notepad is running".
+$ct1 = 0
+
+## Alustame tsüklit, mis kestab seni, kuni Notepad ei tööta.
+do
+{
+$ch = $null
+$ch = Get-Process | ?{$_.name -like "note*"}
+if($ch -ne $null)
+{$ct2 = 2
+Write-Host "Notepad is running"
+Start-Sleep -Seconds 1
+$ct1++
+}
+else
+{$ct2 = 1}
+}
+## Tsükkel kestab seni, kuni $ct2 pole 1.
+while($ct2 -ne 1)
+
+## Näeme, mitu korda Notepad töötas järjest.
+$ct1
