9057af866c6b4246b002d7687e5dcf4cf5cc4f8f
Aleksander Püüa <aleksander.puua@voco.ee>
2025-09-02 15:58:40 +0300

    1. küsimus

---
+++ praks4/praks4.ps1
@@ -0,0 +1,14 @@
+﻿## Koostame value1 ja value2 jaoks küsimuse.
+$value1 = Read-Host "Enter the first value"
+$value2 = Read-host "Enter the second value"
+
+## Teeme tingimuse, kus kui esimene value on väiksem, kui teine, siis kuvame teise numbri.
+
+if($value1 -gt $value2)
+  {
+    Write-Host "The higher number is : "$value1
+  }
+else
+  {
+    Write-Host "The higher number is: "$value2
+  }
