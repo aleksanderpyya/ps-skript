13b4020034a40dce0f29b8ccce4ef062c19e1de0
Aleksander Püüa <aleksander.puua@voco.ee>
2025-09-02 16:17:13 +0300

    1. küsimus

---
+++ praks5/praks5.ps1
@@ -0,0 +1,5 @@
+﻿## Saame praegu käiva programmi kohta infot - nimi ja id.
+Get-Process | ?{$_.ProcessName -eq "notepad"} | Select ProcessName,Id
+
+## Lihtsam võimalus, kuidas seda teha.
+Get-Process Notepad | Select ProcessName,Id
