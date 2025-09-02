e198e1fb1857e4615c5daaa4db7b27a7c40a241d
Aleksander Püüa <aleksander.puua@voco.ee>
2025-09-02 17:14:05 +0300

    "Notepad is running" skript

---
+++ praks8/praks8.ps1
@@ -0,0 +1,5 @@
+﻿## Teeme skripti, kui Notepad töötab ilmuvad teated "Notepad is running" seni kuni selle kinni panen .
+while(Get-Process Notepad -ErrorAction SilentlyContinue)
+  {
+   Write-Host "Notepad is running"
+  }
