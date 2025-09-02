af453f9752b0f81e2bbbf13b4cef1a0d7298597c
Aleksander Püüa <aleksander.puua@voco.ee>
2025-09-02 15:25:19 +0300

    Hash

---
+++ praks2/praks2.ps1
@@ -0,0 +1,13 @@
+﻿## Loome hash1 (DaysWorked) ja hash2 (SalaryPerDay).
+$hash1 = @{Name="John","Joe","Mary";DaysWorked=12,20,18}
+
+$hash2 = @{Name="John","Joe","Mary";SalaryPerDay=100,120,150}
+
+## Arvutame Johni, Joe ja Mary salary.
+$JohnSalary = $hash1.DaysWorked[0] * $hash2.SalaryPerDay[0]
+$JoeSalary = $hash1.DaysWorked[1] * $hash2.SalaryPerDay[1]
+$MarySalary = $hash1.DaysWorked[2] * $hash2.SalaryPerDay[2]
+
+## Teeme ülevaate Johni, Joe ja Mary lõplikkust palgast.
+$hash3 = @{Name=$hash1.Name[0],$hash1.Name[1],$hash1.Name[2];Salary=$JohnSalary,$JoeSalary,$MarySalary}
+$hash3
