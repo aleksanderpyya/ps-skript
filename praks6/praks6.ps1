ad718c89c18cff126265d379930896f2f0663bb7
Aleksander Püüa <aleksander.puua@voco.ee>
2025-09-02 16:44:49 +0300

    CSV faili importimine ja muutmine PowerShellis

---
+++ praks6/praks6.ps1
@@ -0,0 +1,25 @@
+﻿## Importime CSV faili õpilaste andmetega.
+$csv = Import-Csv C:\temp\test\students.csv
+
+## Teeme tulemuse võrrandi.
+$result = @()
+  
+  foreach($c in $csv)
+    {
+      if([int]$c.Age -ge 4 -and [int]$c.Age -le 10)
+       {
+         $school = "Junior"
+       }
+      elseif([int]$c.Age -ge 11 -and [int]$c.Age -le 17)
+       {
+         $school = "Senior"
+       }
+      $temp = [PSCustomObject]@{
+          Name = $c.Name
+          School = $school
+           }
+      $result += $temp
+    }
+
+## Näitame tulemust
+$result
