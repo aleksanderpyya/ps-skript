b3e3d68451dda275ac5605fcd816abdb2a09ce68
Aleksander Püüa <aleksander.puua@voco.ee>
2025-09-02 15:49:52 +0300

    Read-Host ja Write-Host

---
+++ praks3/praks3.ps1
@@ -0,0 +1,5 @@
+﻿## Küsime küsimuse, millele saame vastata.
+$name = Read-Host "What is your name?"
+
+## Paneme paika nime ning vastuse rohelist värvi.
+Write-Host "My name is : $name " -ForegroundColor Green
