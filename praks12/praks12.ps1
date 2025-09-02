38aa2d70f6293db6d94430f8597d0870d743d4c3
Aleksander Püüa <aleksander.puua@voco.ee>
2025-09-02 18:07:39 +0300

    1. küsimus

---
+++ praks12/praks12.ps1
@@ -0,0 +1,21 @@
+﻿## Teenuste oleku jälgimise skript.
+
+
+## Teeme funktsiooni, mille kaudu näeme, mitu teenust on töötavas olekus.
+function countstart
+  {
+    $start = Get-Service | ?{$_.Status -eq "Running"}
+    Write-Host "Total services in running state = "$start.count
+  }
+
+
+## Funktsioon, mille kaudu näeme, mitu teenust on peatunud olekus.
+function countstop
+  {
+    $stop = Get-Service | ?{$_.Status -eq "Stopped"}
+    Write-Host "Total services in stopped state = "$stop.count
+  }
+
+countstart
+
+countstop
