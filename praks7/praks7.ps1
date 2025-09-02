5100aaa9f2320d09e52a9dbe678b42198b0bfe2a
Aleksander Püüa <aleksander.puua@voco.ee>
2025-09-02 17:10:37 +0300

    Roll

---
+++ praks7/praks7.ps1
@@ -0,0 +1,15 @@
+﻿## Teeme erinevad grupid.
+$groups = @("Red","Green","Yellow","Blue")
+
+## Teeme sellise võrrandi, et iga number/õpilane saab suvalise värvi.
+$result = @()
+for($i = 1 ; $i-le 20 ; $i++)
+  {
+    $g = Get-Random $groups
+    $temp = [PSCustomObject]@{
+      RollNumber = $i
+      Group = $g
+       }
+    $result += $temp
+  }
+$result
